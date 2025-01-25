47c81eb353616732283b0681b07fa76ad59b94b1
u210709007 <ilaydagunay46@gmail.com>
2025-01-26 02:36:26 +0300

    food website complete

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const searchForm = document.querySelector(".search-form");
+const cartItems = document.querySelector(".cart-items-container");
+const navbar = document.querySelector(".navbar");
+
+
+//buttons
+const searchBtn = document.querySelector("#search-btn");
+const cartBtn = document.querySelector("#cart-btn");
+const menuBtn = document.querySelector("#menu-btn");
+
+
+searchBtn.addEventListener("click", function(){
+    searchForm.classList.toggle("active");
+    document.addEventListener("click", function(e){
+        if(
+            !e.composedPath().includes(searchBtn) && 
+            !e.composedPath().includes(searchForm)
+        ){
+            searchForm.classList.remove("active");
+        }
+    });
+});  
+
+
+cartBtn.addEventListener("click", function(){
+    cartItems.classList.toggle("active");
+    document.addEventListener("click", function(e){
+        if(
+            !e.composedPath().includes(cartBtn) && 
+            !e.composedPath().includes(cartItems)
+        ){
+            cartItems.classList.remove("active");
+        }
+    });
+});
+
+
+menuBtn.addEventListener("click", function(){
+    navbar.classList.toggle("active");
+    document.addEventListener("click", function(e){
+        if(
+            !e.composedPath().includes(menuBtn) && 
+            !e.composedPath().includes(navbar)
+        ){
+            navbar.classList.remove("active");
+        }
+    });
+}); 
